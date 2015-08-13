01e57eb2639b8562e0ce7daa159ce3dbc5d8160e
chazwarp923 <chazwarp923@icloud.com>
2015-08-13 13:04:54 -0400

    Change to FileHelper to add variety to how it can be used

--- src/com/chazwarp/JWarpCore/File/FileHelper.java
+++ src/com/chazwarp/JWarpCore/File/FileHelper.java
@@ -5,6 +5,21 @@ package com.chazwarp.JWarpCore.File;
 
 public class FileHelper {
 	
+	public static String getSaveDirectory(String subdirectory) {
+		String baseSaveDirectory = null;
+		String OS = (System.getProperty("os.name")).toUpperCase();
+		
+		if(OS.contains("WIN")) {
+			baseSaveDirectory = System.getenv("AppData");
+		}
+		else {
+			baseSaveDirectory = System.getProperty("user.home");
+			baseSaveDirectory += "/Library/Application Support";
+		}
+		
+		return baseSaveDirectory + "\\" + subdirectory;
+	}
+	
 	public static String getSaveDirectory(String subdirectory, String filename) {
 		String baseSaveDirectory = null;
 		String OS = (System.getProperty("os.name")).toUpperCase();
